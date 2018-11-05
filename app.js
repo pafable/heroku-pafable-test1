f6f9ef0c8dc6789a0851756d66b182efb075723f
pafable <39135798+pafable@users.noreply.github.com>
2018-11-04 21:14:18 -0500

    add /bye route

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", function(req,res){
     res.send("Hello World");
 });
 
+app.get("/bye", function(req,res){
+    res.send("Bye World");
+});
+
 app.listen(80, process.env.IP, function(req,res){
     console.log("Server Started");
 });
